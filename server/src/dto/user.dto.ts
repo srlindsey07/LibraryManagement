c7bf87c81d041e3892adb01ccf66df632a07c982
Stephanie Rentfrow <stephanie.rentfrow@gmail.com>
2024-05-24 19:25:37 -0400

    Fix .gitignore

--- server/src/dto/user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from "../entities/User";
-
-export class UserResponse {
-    id: number;
-    first_name: string;
-    last_name: string;
-    role: UserRole;
-    email: string;
-}
